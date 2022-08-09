a0508c0ef74ad9924601716f662885ce788e65cb
iifawzi <iifawzie@gmail.com>
2022-08-09 20:42:57 +0200

    Adding a reference to the kafka client and adding kafka, zookeeper, and kafkaUI to docker compose
    Signed-off-by: iifawzi <iifawzie@gmail.com>

--- auth/src/modules/auth/auth.module.ts
+++ auth/src/modules/auth/auth.module.ts
@@ -7,9 +7,26 @@ import AuthService from "./auth.service";
 import { LocalStrategy } from "./passport/strategies";
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from "@nestjs/config";
+import { ClientsModule, Transport } from "@nestjs/microservices";
 
 @Module({
-    imports: [DatabaseModule,
+    imports: [
+        DatabaseModule,
+        ClientsModule.register([
+            {
+                name: 'MAIL_SERVICE',
+                transport: Transport.KAFKA,
+                options: {
+                    client: {
+                        clientId: 'auth',
+                        brokers: ['localhost:9092'],
+                    },
+                    consumer: {
+                        groupId: 'mail-consumer'
+                    }
+                }
+            }
+        ]),
         JwtModule.registerAsync({
             inject: [ConfigService],
             useFactory: async (configService: ConfigService) => ({
@@ -34,7 +51,7 @@ import { ConfigService } from "@nestjs/config";
         {
             provide: 'bcryptHelpers',
             useClass: BcryptHelpers
-        }
+        },
     ],
     exports: ['AuthLogger', 'AuthRepository', 'bcryptHelpers']
 })
